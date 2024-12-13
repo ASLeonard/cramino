02a3ebdcfb99e843df7e1da9a219ba7b43221540
ASLeonard <alexander.leonard@usys.ethz.ch>
2024-12-13 07:29:50 +0100

    WIP for ubam summary

--- src/extract_from_bam.rs
+++ src/extract_from_bam.rs
@@ -1,36 +1,17 @@
-use bam::ext::BamRecordExtensions;
-use rust_htslib::bam::record::{Aux, Cigar};
-use rust_htslib::{bam, bam::Read, htslib};
-use std::env;
-use url::Url;
+use rust_htslib::bam::record::{Aux};
+use rust_htslib::{bam, bam::Read};
 
 pub struct Data {
     pub lengths: Option<Vec<u128>>,
     pub all_counts: usize,
-    pub identities: Option<Vec<f64>>,
-    pub tids: Option<Vec<i32>>,
-    pub starts: Option<Vec<i64>>,
-    pub ends: Option<Vec<i64>>,
-    pub phasesets: Option<Vec<Option<u32>>>,
-    pub exons: Option<Vec<usize>>,
+    pub qualities: Option<Vec<f64>>,
 }
 
 pub fn extract(args: &crate::Cli) -> (Data, rust_htslib::bam::Header) {
     let mut lengths = vec![];
-    let mut identities = vec![];
-    let mut tids = vec![];
-    let mut starts = vec![];
-    let mut ends = vec![];
-    let mut phasesets = vec![];
-    let mut exons = vec![];
+    let mut qualities = vec![];
     let mut bam = if args.input == "-" {
         bam::Reader::from_stdin().expect("\n\nError reading alignments from stdin.\nDid you include the file header with -h?\n\n\n\n")
-    } else if args.input.starts_with("s3") || args.input.starts_with("https://") {
-        if env::var("CURL_CA_BUNDLE").is_err() {
-            env::set_var("CURL_CA_BUNDLE", "/etc/ssl/certs/ca-certificates.crt");
-        }
-        bam::Reader::from_url(&Url::parse(&args.input).expect("Failed to parse URL"))
-            .unwrap_or_else(|err| panic!("Error opening remote BAM: {err}"))
     } else {
         bam::Reader::from_path(&args.input)
             .expect("Error opening BAM/CRAM file.\nIs the input file correct?\n\n\n\n")
@@ -60,193 +41,36 @@ pub fn extract(args: &crate::Cli) -> (Data, rust_htslib::bam::Header) {
     bam.set_threads(args.threads)
         .expect("Failure setting decompression threads");
 
-    let min_read_len = args.min_read_len;
-    // the match statement below is a bit ugly, but it is the only way to get a closure
-    // that closure is used for filtering the reads
-    // the closure is different depending on inclusion of unmapped reads (--ubam) and the minimum read length (--min-read-len)
-    let filter_closure: Box<dyn Fn(&bam::Record) -> bool> = match (args.ubam, args.min_read_len) {
-        (false, 0) => Box::new(|record: &bam::Record| {
-            // filter out unmapped, no length filter
-            record.flags() & htslib::BAM_FUNMAP as u16 == 0
-        }),
-        (false, l) if l > 0 => Box::new(|record: &bam::Record| {
-            // filter out unmapped, with a length filter
-            record.flags() & htslib::BAM_FUNMAP as u16 == 0
-                && record.seq_len() > min_read_len
-        }),
-        // keep unmapped reads, no length filter
-        (true, 0) => Box::new(|_: &bam::Record| true),
-        (true, l) if l > 0 => Box::new(|record: &bam::Record| {
-            // only length filter, keep unmapped
-            record.seq_len() > min_read_len
-        }),
-        // the pattern below should be unreachable, as the min_read_len is either zero or positive
-        (false, _) | (true, _) => unreachable!(),
-    };
     let mut all_counts = 0;
     for read in bam
         .rc_records()
         .map(|r| r.expect("Failure parsing Bam file"))
-        .filter(|record| record.flags() & (htslib::BAM_FSECONDARY) as u16 == 0)
         .inspect(|_| all_counts += 1)
-        .filter(|read| filter_closure(read))
     {
-        lengths.push(read.seq_len() as u128 - softclipped_bases(&read));
-        
-        if args.karyotype || args.phased {
-            tids.push(read.tid());
-        }
-        if args.phased {
-            starts.push(read.pos());
-            ends.push(read.reference_end());
-            phasesets.push(get_phaseset(&read));
-        }
-        if args.spliced {
-            exons.push(get_exon_number(&read));
-        }
-        if !args.ubam {
-            identities.push(gap_compressed_identity(read));
-        }
-    }
-    if let Some(s) = &args.arrow {
-        match args.ubam {
-            true => crate::feather::save_as_arrow_ubam(
-                s.to_string(),
-                lengths.clone().iter().map(|x| *x as u64).collect(),
-            ),
-            false => crate::feather::save_as_arrow(
-                s.to_string(),
-                lengths.clone().iter().map(|x| *x as u64).collect(),
-                identities.clone(),
-            ),
-        }
+        lengths.push(read.seq_len() as u128);
+        qualities.push(get_rq_tag(&read).into());
     }
     // sort vectors in descending order (required for N50/N75)
     lengths.sort_unstable_by(|a, b| b.cmp(a));
-    identities.sort_unstable_by(|a, b| b.partial_cmp(a).unwrap());
     (
         Data {
             lengths: Some(lengths),
             all_counts,
-            identities: if !args.ubam { Some(identities) } else { None },
-            tids: if args.karyotype || args.phased {
-                Some(tids)
-            } else {
-                None
+            qualities: Some(qualities),
             },
-            starts: if args.phased { Some(starts) } else { None },
-            ends: if args.phased { Some(ends) } else { None },
-            phasesets: if args.phased { Some(phasesets) } else { None },
-            exons: if args.spliced { Some(exons) } else { None },
-        },
         header,
     )
 }
 
-/// Calculates the gap-compressed identity
-/// based on https://lh3.github.io/2018/11/25/on-the-definition-of-sequence-identity
-/// recent minimap2 version have that as the de tag
-/// if that is not present it is calculated from CIGAR and NM
-fn gap_compressed_identity(record: std::rc::Rc<rust_htslib::bam::Record>) -> f64 {
-    match get_de_tag(&record) {
-        Some(v) => v as f64,
-        None => {
-            let mut matches = 0;
-            let mut gap_size = 0;
-            let mut gap_count = 0;
-            for entry in record.cigar().iter() {
-                match entry {
-                    Cigar::Match(len) | Cigar::Equal(len) | Cigar::Diff(len) => {
-                        matches += *len;
-                    }
-                    Cigar::Del(len) | Cigar::Ins(len) => {
-                        gap_size += *len;
-                        gap_count += 1;
-                    }
-                    _ => (),
-                }
-            }
-            100.0
-                * (1.0
-                    - ((get_nm_tag(&record) - gap_size + gap_count) as f64
-                        / (matches + gap_count) as f64))
-        }
-    }
-}
 
-fn get_nm_tag(record: &bam::Record) -> u32 {
-    match record.aux(b"NM") {
+
+fn get_rq_tag(record: &bam::Record) -> f32 {
+    match record.aux(b"rq") {
         Ok(value) => match value {
-            Aux::U8(v) => u32::from(v),
-            Aux::U16(v) => u32::from(v),
-            Aux::U32(v) => v,
-            Aux::I8(v) => u32::try_from(v).expect("Identified a negative NM tag"),
-            Aux::I16(v) => u32::try_from(v).expect("Identified a negative NM tag"),
-            Aux::I32(v) => u32::try_from(v).expect("Identified a negative NM tag"),
-            _ => panic!("Unexpected type of Aux for NM tag: {:?}", value),
+            Aux::Float(v) =>
+                v,
+		_ => todo!()
         },
-        Err(_e) => panic!("Unexpected result while trying to access the NM tag"),
+        Err(_e) => -1.0,
     }
-}
-
-/// Get the de:f tag from minimap2, which is the gap compressed sequence divergence
-/// Which is converted into percent identity with 100 * (1 - de)
-/// This tag can be absent if the aligner version is not quite recent
-fn get_de_tag(record: &bam::Record) -> Option<f32> {
-    match record.aux(b"de") {
-        Ok(value) => match value {
-            Aux::Float(v) => Some(100.0 * (1.0 - v)),
-            _ => panic!("Unexpected type of Aux for de tag: {:?}", value),
-        },
-        Err(_e) => None,
-    }
-}
-
-fn get_phaseset(record: &bam::Record) -> Option<u32> {
-    match record.aux(b"PS") {
-        Ok(value) => match value {
-            Aux::U8(v) => Some(u32::from(v)),
-            Aux::U16(v) => Some(u32::from(v)),
-            Aux::U32(v) => Some(v),
-            Aux::I8(v) => Some(u32::try_from(v).unwrap_or_else(|_| {
-                panic!(
-                    "Invalid: Identified a negative PS tag at {}",
-                    std::str::from_utf8(record.qname())
-                        .expect("Failed to convert read name to string")
-                )
-            })),
-            Aux::I16(v) => Some(u32::try_from(v).unwrap_or_else(|_| {
-                panic!(
-                    "Invalid: Identified a negative PS tag at {}",
-                    std::str::from_utf8(record.qname())
-                        .expect("Failed to convert read name to string")
-                )
-            })),
-            Aux::I32(v) => Some(u32::try_from(v).unwrap_or_else(|_| {
-                panic!(
-                    "Invalid: Identified a negative PS tag at {}",
-                    std::str::from_utf8(record.qname())
-                        .expect("Failed to convert read name to string")
-                )
-            })),
-            _ => panic!("Unexpected type of Aux for phaseset: {:?}", value),
-        },
-        Err(_e) => None,
-    }
-}
-
-fn get_exon_number(record: &bam::Record) -> usize {
-    let mut exon_count = 1;
-
-    for op in record.cigar().iter() {
-        if let Cigar::RefSkip(_len) = op {
-            exon_count += 1;
-        }
-    }
-
-    exon_count
-}
-
-fn softclipped_bases(read: &bam::Record) -> u128 {
-    (read.cigar().leading_softclips() + read.cigar().trailing_softclips()) as u128
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 use clap::Parser;
 use extract_from_bam::Data;
-use log::{error, info};
 use std::path::PathBuf;
 
 pub mod calculations;
 pub mod extract_from_bam;
-pub mod feather;
-pub mod file_info;
-pub mod histograms;
-pub mod karyotype;
-pub mod phased;
-pub mod splicing;
+//pub mod feather;
+//pub mod file_info;
+//pub mod histograms;
+//pub mod karyotype;
+//pub mod phased;
+//pub mod splicing;
 pub mod utils;
 
 // The arguments end up in the Cli struct
@@ -29,47 +28,9 @@ pub struct Cli {
     #[clap(long, value_parser)]
     reference: Option<String>,
 
-    /// Minimal length of read to be considered
-    #[clap(short, long, value_parser, default_value_t = 0)]
-    min_read_len: usize,
-
-    /// If histograms have to be generated
-    #[clap(long, value_parser)]
-    hist: bool,
-
-    /// If a checksum has to be calculated [DEPRECATED]
-    #[clap(long, value_parser)]
-    checksum: bool,
-
-    /// Write data to an arrow format file
-    #[clap(long, value_parser)]
-    arrow: Option<String>,
-
-    /// Provide normalized number of reads per chromosome
-    #[clap(long, value_parser)]
-    karyotype: bool,
-
-    /// Calculate metrics for phased reads
-    #[clap(long, value_parser)]
-    phased: bool,
-
-    /// Provide metrics for spliced data
-    #[clap(long, value_parser)]
-    spliced: bool,
-
-    /// Provide metrics for unaligned reads
-    #[clap(long, value_parser)]
-    ubam: bool,
 }
 
 pub fn is_file(pathname: &str) -> Result<(), String> {
-    if pathname == "-"
-        || pathname.starts_with("http")
-        || pathname.starts_with("ftp")
-        || pathname.starts_with("s3")
-    {
-        return Ok(());
-    }
     let path = PathBuf::from(pathname);
     if path.is_file() {
         Ok(())
@@ -79,92 +40,36 @@ pub fn is_file(pathname: &str) -> Result<(), String> {
 }
 
 fn main() -> Result<(), rust_htslib::errors::Error> {
-    env_logger::init();
-    let mut args = Cli::parse();
-    is_file(&args.input).unwrap_or_else(|_| panic!("Path to input file {} is invalid", args.input));
-    if args.ubam {
-        args.karyotype = false;
-        args.phased = false;
-        args.spliced = false;
-    };
-    if args.checksum {
-        eprintln!(
-            "Calculating a checksum is deprecated, let me you if you disagree and want it back"
-        );
-    }
-    info!("Collected arguments");
-    let (metrics, header) = extract_from_bam::extract(&args);
+   let args = Cli::parse(); 
+   let (metrics, header) = extract_from_bam::extract(&args);
+  
+   for (key, value) in header.to_hashmap().into_iter() {
+      println!("{} / {:?}", key, value);
+   }
 
-    metrics_from_bam(metrics, args, header)?;
-    info!("Finished");
-    Ok(())
+   metrics_from_bam(metrics)?;
+   Ok(())
 }
 
 fn metrics_from_bam(
     metrics: Data,
-    args: Cli,
-    header: rust_htslib::bam::Header,
 ) -> Result<(), rust_htslib::errors::Error> {
-    let bam = file_info::BamFile { path: args.input };
-    println!("File name\t{}", bam.file_name());
 
-    let genome_size = utils::get_genome_size(&header)?;
     generate_main_output(
         metrics.lengths.as_ref().unwrap(),
-        metrics.identities.as_ref(),
-        genome_size,
+        metrics.qualities.as_ref().unwrap(),
         metrics.all_counts,
     );
 
-    println!("Path\t{}", bam);
-    println!("Creation time\t{}", bam.file_time());
-
-    let phaseblocks = if args.phased {
-        Some(phased::phase_metrics(
-            metrics.tids.as_ref().unwrap(),
-            metrics.starts.unwrap(),
-            metrics.ends.unwrap(),
-            metrics.phasesets.as_ref().unwrap(),
-        ))
-    } else {
-        None
-    };
-    if args.karyotype {
-        karyotype::make_karyotype(metrics.tids.as_ref().unwrap(), header);
-    }
-    let exon_counts = if let Some(mut exon_counts) = metrics.exons {
-        exon_counts.sort_unstable();
-        splicing::splice_metrics(&exon_counts);
-        exon_counts
-    } else {
-        Vec::new()
-    };
-    if args.hist {
-        histograms::make_histogram_lengths(metrics.lengths.as_ref().unwrap());
-        if !args.ubam {
-            histograms::make_histogram_identities(metrics.identities.as_ref().unwrap());
-        }
-        if args.phased {
-            histograms::make_histogram_phaseblocks(&phaseblocks.unwrap())
-        }
-        if args.spliced {
-            histograms::make_histogram_exons(&exon_counts);
-        }
-    }
     Ok(())
 }
 
 fn generate_main_output(
     lengths: &Vec<u128>,
-    identities: Option<&Vec<f64>>,
-    genome_size: u64,
+    qualities: &Vec<f64>,
     all_reads: usize,
 ) {
     let num_reads = lengths.len();
-    if num_reads < 2 {
-        error!("Not enough reads to calculate metrics!");
-        panic!();
-    }
     let data_yield: u128 = lengths.iter().sum::<u128>();
     println!("Number of alignments\t{num_reads}");
     println!(
@@ -172,118 +77,11 @@ fn generate_main_output(
         (num_reads as f64) / (all_reads as f64) * 100.0
     );
     println!("Yield [Gb]\t{:.2}", data_yield as f64 / 1e9);
-    println!(
-        "Mean coverage\t{:.2}",
-        data_yield as f64 / genome_size as f64
-    );
     let data_yield_long = lengths.iter().filter(|l| l > &&25000).sum::<u128>();
     println!("Yield [Gb] (>25kb)\t{:.2}", data_yield_long as f64 / 1e9);
+
+
     println!("N50\t{}", calculations::get_n(lengths, data_yield, 0.50));
-    println!("N75\t{}", calculations::get_n(lengths, data_yield, 0.75));
-    println!("Median length\t{:.2}", calculations::median_length(lengths));
-    println!("Mean length\t{:.2}", data_yield / num_reads as u128);
-    if let Some(identities) = identities {
-        println!("Median identity\t{:.2}", calculations::median(identities));
-        println!(
-            "Mean identity\t{:.2}",
-            identities.iter().sum::<f64>() / (num_reads as f64)
-        );
-        // modal accuracy has lower precision because it gets inflated and divided by 10, losing everything after the first decimal
-        println!(
-            "Modal identity\t{:.1}",
-            calculations::modal_accuracy(identities)
-        );
-    }
-}
-
-#[cfg(test)]
-#[ctor::ctor]
-fn init() {
-    env_logger::init();
-}
-
-#[test]
-fn verify_app() {
-    use clap::CommandFactory;
-    Cli::command().debug_assert()
-}
-
-#[test]
-fn extract() {
-    let args = Cli {
-        input: "test-data/small-test-phased.bam".to_string(),
-        threads: 8,
-        reference: None,
-        min_read_len: 0,
-        hist: true,
-        checksum: false,
-        arrow: Some("test.feather".to_string()),
-        karyotype: true,
-        phased: true,
-        spliced: false,
-        ubam: false,
-    };
-    let (metrics, header) = extract_from_bam::extract(&args);
-    assert!(metrics_from_bam(metrics, args, header).is_ok())
-}
-
-// this test is ignored because it uses a local reference file
-#[ignore]
-#[test]
-fn extract_cram() {
-    let args = Cli {
-        input: "test-data/small-test-phased.cram".to_string(),
-        threads: 8,
-        reference: Some("/home/wdecoster/reference/GRCh38.fa".to_string()),
-        min_read_len: 0,
-        hist: false,
-        checksum: false,
-        arrow: None,
-        karyotype: false,
-        phased: false,
-        spliced: false,
-        ubam: false,
-    };
-    let (metrics, header) = extract_from_bam::extract(&args);
-    assert!(metrics_from_bam(metrics, args, header).is_ok())
-}
-
-#[test]
-fn extract_ubam() {
-    let args = Cli {
-        input: "test-data/small-test-ubam.bam".to_string(),
-        threads: 8,
-        reference: None,
-        min_read_len: 0,
-        hist: true,
-        checksum: false,
-        arrow: Some("test.feather".to_string()),
-        karyotype: false,
-        phased: false,
-        spliced: false,
-        ubam: true,
-    };
-    let (metrics, header) = extract_from_bam::extract(&args);
-    assert!(metrics_from_bam(metrics, args, header).is_ok())
-}
-
-// this test is ignored because it uses a local reference file and takes a very long time
-#[ignore]
-#[test]
-fn extract_url() {
-    let args = Cli {
-        input: "https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1KG_ONT_VIENNA/hg38/HG00096.hg38.cram".to_string(),
-        threads: 8,
-        reference: Some("/home/wdecoster/local/1KG_ONT_VIENNA_hg38.fa.gz".to_string()),
-        min_read_len: 0,
-        hist: true,
-        checksum: false,
-        arrow: None,
-        karyotype: false,
-        phased: false,
-        spliced: false,
-        ubam: false,
-    };
-    let (metrics, header) = extract_from_bam::extract(&args);
-    assert!(metrics_from_bam(metrics, args, header).is_ok())
+    println!("QV \t{:.2}", qualities.iter().sum::<f64>() / qualities.len() as f64);   
+ 
 }
